1c31fed71c500f9ef45310378caf23a5002513fb
GRosza <grzegorz.roszak@coderscrew.pl>
2022-01-13 00:49:02 +0100

    added minor fix in styles

--- frontend/src/views/HomePage/HomePage.jsx
+++ frontend/src/views/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import 'typeface-roboto';
 import Button from '@mui/material/Button';
 import React from 'react';
 
-import styles from '../../components/Button/button.module.scss';
+import buttoStyles from '../../components/Button/button.module.scss';
 import Clock from '../../components/Clock/Clock';
 import Date from '../../components/Date/Date';
 
@@ -13,23 +13,23 @@ function homePage() {
     <div className="homeContainer">
       <div className="textContetn">
         <div className="title">
-          <h1>AID KIT</h1>
+          <h1>@ID kIT</h1>
           <h2>Your medical assistant</h2>
         </div>
         <ul className="navList">
-          <Button variant="contained" className={styles.Button}>
+          <Button variant="contained" className={buttoStyles.Button}>
             DAILY DRUGS
           </Button>
           <br />
-          <Button variant="contained" className={styles.Button}>
+          <Button variant="contained" className={buttoStyles.Button}>
             CREDITS
           </Button>
           <br />
-          <Button variant="contained" className={styles.Button}>
+          <Button variant="contained" className={buttoStyles.Button}>
             LEXICON
           </Button>
           <br />
-          <Button variant="contained" className={styles.Button}>
+          <Button variant="contained" className={buttoStyles.Button}>
             SETTINGS
           </Button>
         </ul>
