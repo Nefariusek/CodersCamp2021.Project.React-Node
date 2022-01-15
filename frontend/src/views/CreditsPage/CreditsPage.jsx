83aeab304816db72855d33eea6a2193f3d50ab77
NataliaCichonska <natalia.cichonska1103@gmail.com>
2022-01-15 12:33:53 +0100

    add array.map change

--- frontend/src/views/CreditsPage/CreditsPage.jsx
+++ frontend/src/views/CreditsPage/CreditsPage.jsx
@@ -5,50 +5,38 @@ import Button from '@mui/material/Button';
 
 import buttonStyles from '../../components/Button/Button.module.scss';
 
+const ButtonsComponent = () => {
+  const developers = [
+    { href: 'https://github.com/Nefariusek', name: 'Szymon' },
+    { href: 'https://github.com/mngweb', name: 'Sylwia' },
+    { href: 'https://github.com/NataliaCichonska', name: 'Natalia' },
+    { href: 'https://github.com/MariaBanaszkiewicz', name: 'Maria' },
+    { href: 'https://github.com/Urszuja', name: 'Ula' },
+    { href: 'https://github.com/GRosza', name: 'Grzegorz' },
+  ];
+  return (
+    <>
+      {developers.map(({ href, name }) => (
+        <Button variant="contained" className={buttonStyles.Button} target="_blank" href={href}>
+          {name}
+        </Button>
+      ))}
+    </>
+  );
+};
+
 const CreditsPage = () => {
   return (
     <div className="creditsPage">
-      <Typography className="header" variant="h2">
+      <Typography className="header-h2" variant="h2">
         Second project of Coders Camp 2021
       </Typography>
-      <Typography className="header" variant="h3">
+      <Typography className="header-h3" variant="h3">
         Meet our team!
       </Typography>
-      <div className="container">
-        <div className="githubButtons">
-          <Button
-            variant="contained"
-            className={buttonStyles.Button}
-            href="https://github.com/Nefariusek"
-            target="_blank"
-          >
-            Szymon
-          </Button>
-          <Button variant="contained" className={buttonStyles.Button} href="https://github.com/mngweb" target="_blank">
-            Sylwia
-          </Button>
-          <Button
-            variant="contained"
-            className={buttonStyles.Button}
-            href="https://github.com/NataliaCichonska"
-            target="_blank"
-          >
-            Natalia
-          </Button>
-          <Button
-            variant="contained"
-            className={buttonStyles.Button}
-            href="https://github.com/MariaBanaszkiewicz"
-            target="_blank"
-          >
-            Maria
-          </Button>
-          <Button variant="contained" className={buttonStyles.Button} href="https://github.com/GRosza" target="_blank">
-            Grzegorz
-          </Button>
-          <Button variant="contained" className={buttonStyles.Button} href="https://github.com/Urszuja" target="_blank">
-            Ula
-          </Button>
+      <div className="credits-page-container">
+        <div className="github-buttons">
+          <ButtonsComponent />
         </div>
         <img className="creditsPageImg" src="./creditsPills.png" alt="credits page img" />
       </div>
