db2ea9c50938b5b4bc5390b3fde44afe10e54292
Urszuja <urszula.jarych@gmail.com>
2022-01-18 18:29:34 +0100

    add basic router

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,13 +2,51 @@ import './sass/styles.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
-import App from './views/App/App';
+import CreditsPage from './views/CreditsPage/CreditsPage';
+import HomePage from './views/HomePage/HomePage';
+import LoginPage from './views/LoginPage/LoginPage';
+import SignUpPage from './views/SignUpPage/SignUpPage';
+
+const PATHS = [
+  { url: '/', element: <HomePage /> },
+  { url: '/login', element: <LoginPage /> },
+  { url: '/register', element: <SignUpPage /> },
+  // {url: '/userhub', element: <Userhub/>},
+  // {url: '/dailydrugs', element: <DailyDrugsPage />},
+  // {url: '/calendar', element: <Calendar/>},
+  // {url: '/lexicon', element: <LexiconPage />},
+  { url: '/credits', element: <CreditsPage /> },
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {PATHS.map((path) => (
+          <Route path={path.url} element={path.element} />
+        ))}
+        ;
+        {/*
+          <Route path="/" element={<WelcomePage />}>  
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/userhub" element={<Userhub />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/dailydrugs" element={<DailyDrugsPage />} />
+          <Route path="/lexicon" element={<LexiconPage />} /> */}
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>Page not found</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
