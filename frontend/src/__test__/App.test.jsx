a67bb30ce680aee7ddffbffe44ec810029a42c8b
Urszuja <urszula.jarych@gmail.com>
2022-01-18 19:31:36 +0100

    Revert "Merge pull request #10 from Nefariusek/feature/issue6"
    This reverts commit 6638a00b31332afdb0ded1af9f270bd0b621f719, reversing
    changes made to 464adada19d3fbacc78e3c35f9f554bb9da771d9.

--- frontend/src/__test__/App.test.jsx
+++ frontend/src/__test__/App.test.jsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 
 import App from '../views/App/App';
 
-test('renders navbar', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const navElement = screen.queryAllByText(/aID kIT/i)[0];
-  expect(navElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- frontend/src/views/App/App.jsx
+++ frontend/src/views/App/App.jsx
@@ -1,11 +1,19 @@
 import './App.scss';
 
-import Navbar from '../../components/Navbar/Navbar';
+import Button from '@mui/material/Button';
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+        <Button variant="text">Text</Button>
+      </header>
     </div>
   );
 };
