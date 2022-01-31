998103ef4d67b49096d8223d5740a8edc98d4d48
NataliaCichonska <natalia.cichonska1103@gmail.com>
2022-01-31 10:51:53 +0100

    change mathod for array

---
+++ frontend/src/mock/drugs.js
@@ -0,0 +1,28 @@
+const drugs = [
+  {
+    type: 'pills',
+    name: 'pills',
+    expirationDate: '01.01.2023',
+    notes: 'pills for headache',
+    quantity: '10',
+    photoSource: '/creditsPills.png',
+  },
+  {
+    type: 'syrup',
+    name: 'syrup',
+    expirationDate: '12.02.2021',
+    notes: 'syrup for sorethroat',
+    quantity: '4',
+    photoSource: '/apteczka.png',
+  },
+  {
+    type: 'patches',
+    name: 'patches',
+    expirationDate: '20.02.2021',
+    notes: 'patches for bleeding and other small injuries',
+    quantity: '10',
+    photoSource: '/creditsPills.png',
+  },
+];
+
+export default drugs;
--- frontend/src/views/MyDrugsPage/MyDrugsPage.jsx
+++ frontend/src/views/MyDrugsPage/MyDrugsPage.jsx
@@ -5,47 +5,20 @@ import { Button, Typography } from '@mui/material';
 import React, { useState } from 'react';
 
 import buttonStyles from '../../components/Button/button.module.scss';
+import drugs from '../../mock/drugs';
 
-const drugs = [
-  {
-    type: 'pills',
-    name: 'tabletki',
-    expirationDate: '01.01',
-    notes: 'tabletki na ból głowy',
-    quantity: '10',
-    photoSource: '/creditsPills.png',
-  },
-  {
-    type: 'syrup',
-    name: 'syrop',
-    expirationDate: '02.02',
-    notes: 'syrop na kaszel',
-    quantity: '4',
-    photoSource: '/apteczka.png',
-  },
-  {
-    type: 'patches',
-    name: 'plastry',
-    expirationDate: '04.04',
-    notes: 'Plastry na skaleczenia',
-    quantity: '10',
-    photoSource: '/creditsPills.png',
-  },
-];
+const initialDescription = 'CHOOSE A DRUG TO SHOW THE DESCRIPTION';
+const initialImageSource = '/apteczka.png';
 
 const MyDrugsPage = () => {
-  const [description, setDescription] = useState('CHOOSE A DRUG TO SHOW THE DESCRIPTION');
-  const [photoSource, setPhotoSource] = useState('/apteczka.png');
+  const [description, setDescription] = useState(initialDescription);
+  const [photoSource, setPhotoSource] = useState(initialImageSource);
 
   const showDescription = (selectedDrug) => {
-    drugs.forEach((drug) => {
-      const drugArray = Object.entries(drug);
-      if (selectedDrug.target.innerText === drugArray[1][1].toUpperCase()) {
-        const readyDescription = `NAME: ${drugArray[1][1]} \n EXPIRATION DATE: ${drugArray[2][1]} \n INFO: ${drugArray[3][1]} \n QUANTITY: ${drugArray[4][1]}`;
-        setDescription(readyDescription);
-        setPhotoSource(drugArray[5][1]);
-      }
-    });
+    const foundDrug = drugs.find(({ name }) => name.toUpperCase() === selectedDrug.target.innerText);
+    const readyDescription = `NAME: ${foundDrug.name} \n EXPIRATION DATE: ${foundDrug.expirationDate} \n INFO: ${foundDrug.notes} \n QUANTITY: ${foundDrug.quantity}`;
+    setDescription(readyDescription);
+    setPhotoSource(foundDrug.photoSource);
   };
 
   return (
