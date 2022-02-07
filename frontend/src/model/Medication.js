a2476aa22eba4713bbba725212536cec99c1a4b6
MariaBanaszkiewicz <xmox@interia.pl>
2022-02-08 00:54:00 +0100

    improve formating getExpirationDate method

--- frontend/src/model/Medication.js
+++ frontend/src/model/Medication.js
@@ -13,9 +13,11 @@ export default class Medication {
 
   getExpirationDate() {
     let day = this.expirationDate.getDate();
-    day = day > 9 ? day : `0${day}`;
     let month = this.expirationDate.getMonth() + 1;
+
+    day = day > 9 ? day : `0${day}`;
     month = month > 9 ? month : `0${month}`;
+
     return `${day}.${month}.${this.expirationDate.getFullYear()}`;
   }
 
