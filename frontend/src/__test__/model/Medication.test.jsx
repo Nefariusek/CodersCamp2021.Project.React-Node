57625dd746908385d61fe54ffc764ba5c33481f9
Urszuja <urszula.jarych@gmail.com>
2022-02-08 18:05:41 +0100

    add test for med daytime in medication

--- frontend/src/__test__/model/Medication.test.jsx
+++ frontend/src/__test__/model/Medication.test.jsx
@@ -8,16 +8,18 @@ const testDrug = new Medication(
   10,
   'testDescription',
   'testImage',
+  ['Noon'],
 );
 
 test('Medication class has all atributes', () => {
   expect(testDrug.name).toBe('testName');
   expect(testDrug.expirationDate).toStrictEqual(new Date(2022, 2, 10));
-  expect(testDrug.category).toBe('testCategory');
+  expect(testDrug.type).toBe('testCategory');
   expect(testDrug.dosage).toBe(1);
   expect(testDrug.quantity).toBe(10);
   expect(testDrug.description).toBe('testDescription');
   expect(testDrug.img).toBe('testImage');
+  expect(testDrug.daytime).toEqual(['Noon']);
 });
 
 test('getExpirationDate method returns date in dd.mm.yyyy format', () => {
