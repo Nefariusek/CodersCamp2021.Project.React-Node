0d515d01283a3e3a1274cc134ccaf5da038e7457
MariaBanaszkiewicz <xmox@interia.pl>
2022-02-19 21:57:32 +0100

    add settings models tests

--- backend/src/__test__/models/Settings.test.js
+++ backend/src/__test__/models/Settings.test.js
@@ -3,7 +3,25 @@ import Settings from '../../models/Settings';
 describe('Settings model', () => {
   it('new settings has default values', () => {
     const testSettings = new Settings();
+
     expect(testSettings.appTheme).toBe('light');
     expect(testSettings.soonExpLenght).toBe(3);
+    expect(testSettings.validateSync()).toBeUndefined();
+  });
+
+  it('settings model doesnt allow wrong values', () => {
+    const settingsWrongTheme = new Settings({ appTheme: 'wrongAppTheme' });
+    const settingsWrongSoonExpLen = new Settings({ soonExpLenght: 11 });
+
+    expect(settingsWrongTheme.validateSync).toThrow();
+    expect(settingsWrongSoonExpLen.validateSync).toThrow();
+  });
+
+  it('settings model accepts allowed values', () => {
+    const testSettings = new Settings({ appTheme: 'dark', soonExpLenght: 5 });
+
+    expect(testSettings.appTheme).toBe('dark');
+    expect(testSettings.soonExpLenght).toBe(5);
+    expect(testSettings.validateSync()).toBeUndefined();
   });
 });
