ff182cd5e1811fa77d16d86f2f826ae204fb5cf0
MariaBanaszkiewicz <xmox@interia.pl>
2022-02-20 13:00:36 +0100

    fix bugs in settings model

--- backend/src/__test__/models/Settings.test.js
+++ backend/src/__test__/models/Settings.test.js
@@ -1,27 +1,32 @@
 import Settings from '../../models/Settings';
+import { LIGHT_THEME, DARK_THEME } from '../../constants/themes';
+
+let testSettings;
 
 describe('Settings model', () => {
-  it('new settings has default values', () => {
-    const testSettings = new Settings();
+  beforeEach(() => {
+    testSettings = new Settings();
+  });
 
-    expect(testSettings.appTheme).toBe('light');
-    expect(testSettings.soonExpLenght).toBe(3);
+  it('new settings has default values', () => {
+    expect(testSettings.appTheme).toBe(LIGHT_THEME);
+    expect(testSettings.soonExpiringFilterLength).toBe(3);
     expect(testSettings.validateSync()).toBeUndefined();
   });
 
   it('settings model doesnt allow wrong values', () => {
-    const settingsWrongTheme = new Settings({ appTheme: 'wrongAppTheme' });
-    const settingsWrongSoonExpLen = new Settings({ soonExpLenght: 11 });
+    testSettings.appTheme = 'wrongAppTheme';
+    testSettings.soonExpiringFilterLength = 11;
 
-    expect(settingsWrongTheme.validateSync).toThrow();
-    expect(settingsWrongSoonExpLen.validateSync).toThrow();
+    expect(testSettings.validateSync).toThrow();
   });
 
   it('settings model accepts allowed values', () => {
-    const testSettings = new Settings({ appTheme: 'dark', soonExpLenght: 5 });
+    testSettings.appTheme = DARK_THEME;
+    testSettings.soonExpiringFilterLength = 5;
 
-    expect(testSettings.appTheme).toBe('dark');
-    expect(testSettings.soonExpLenght).toBe(5);
+    expect(testSettings.appTheme).toBe(DARK_THEME);
+    expect(testSettings.soonExpiringFilterLength).toBe(5);
     expect(testSettings.validateSync()).toBeUndefined();
   });
 });
--- backend/src/models/Settings.js
+++ backend/src/models/Settings.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import { LIGHT_THEME, DARK_THEME } from '../constants/themes';
 
 const settingsSchema = new mongoose.Schema({
-  appTheme: { type: String, enum: ['light', 'dark'], default: 'light' },
-  soonExpLenght: { type: Number, enum: [3, 5, 7], default: 3 },
+  appTheme: { type: String, enum: [LIGHT_THEME, DARK_THEME], default: LIGHT_THEME },
+  soonExpiringFilterLength: { type: Number, enum: [3, 5, 7], default: 3 },
 });
 
 const Settings = mongoose.model('Settings', settingsSchema);
