c854e99197648dd276d2ae5178ae0443f790c78f
Sylwia <mangoweb@wp.pl>
2022-02-21 16:16:42 +0100

    Add validations.js for validation constants

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,24 +1,6 @@
 import mongoose from 'mongoose';
 import uniqueValidator from 'mongoose-unique-validator';
-
-const RegExpressions = {
-  USERNAME: /^[a-zA-Z0-9]+$/,
-  EMAIL:
-    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i,
-  PASSWORD: /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/,
-};
-
-const RequiredMessages = {
-  USERNAME: 'Username is required',
-  EMAIL: 'Email is required',
-  PASSWORD: 'Password is required',
-};
-
-const NotMatchMessages = {
-  USERNAME: 'Username must have only letters and digits',
-  EMAIL: 'Email is invalid',
-  PASSWORD: 'Password must include at least one uppercase letter, number and special character',
-};
+import { RegExpressions, RequiredMessages, NotMatchMessages } from '../constants/validations.js';
 
 const userSchema = new mongoose.Schema(
   {
