0608b6126c981108c252c2125bd431ae2ae1e9c3
Urszuja <urszula.jarych@gmail.com>
2022-02-21 21:57:58 +0100

    add joi validator to medication category tests

--- backend/src/__test__/models/MedicationCategory.test.js
+++ backend/src/__test__/models/MedicationCategory.test.js
@@ -1,9 +1,12 @@
-import MedicationCategory from '../../models/MedicationCategory';
+import MedicationCategory, { medicationCategoryValidator } from '../../models/MedicationCategory';
 import { PILLS_COLOR, SYRUP_COLOR } from '../../constants/MedicationCategory/pillColors';
 import { QUANTITY_UNIT, VOLUME_UNIT } from '../../constants/MedicationCategory/medsUnits';
 import { PILLS_ICON, SYRUP_ICON } from '../../constants/MedicationCategory/medsIcons';
 
 let testMedicationCategory;
+let testRequestBody;
+let res;
+let err;
 
 describe('MedicationCategory model', () => {
   beforeEach(() => {
@@ -26,5 +29,44 @@ describe('MedicationCategory model', () => {
     expect(testMedicationCategory.unit).toBe(VOLUME_UNIT);
     expect(testMedicationCategory.color).toBe(SYRUP_COLOR);
     expect(testMedicationCategory.icon).toBe(SYRUP_ICON);
+  });
+});
+
+escribe('Joi validator for medication category model', () => {
+  beforeEach(() => {
+    err = undefined;
+    testRequestBody = {
+      body: {
+        name: undefined,
+        unit: undefined,
+        color: undefined,
+        icon: undefined,
+      },
+    };
+  });
+
+  it('Joi validator accepts allowed data', () => {
+    testRequestBody.body.name = 'Xanax';
+    testRequestBody.body.unit = VOLUME_UNIT;
+    testRequestBody.body.color = SYRUP_COLOR;
+    testRequestBody.body.icon = SYRUP_ICON;
+
+    settingsValidator(testRequestBody, res, (e) => {
+      err = e;
+    });
+    expect(err).toBeUndefined();
+  });
+
+  it('Joi validator rejects wrong values', () => {
+    let res;
+    testRequestBody.body.name = 'Xa';
+    testRequestBody.body.unit = 'cm3';
+    testRequestBody.body.color = 'salmon';
+    testRequestBody.body.icon = 'not a valid icon';
+
+    settingsValidator(testRequestBody, res, (e) => {
+      err = e;
+    });
+    expect(err).not.toBeUndefined();
   });
 });
