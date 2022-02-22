4e4854cf92b2ab50bb795bbafe3aa0f3854d5853
MariaBanaszkiewicz <xmox@interia.pl>
2022-02-22 18:39:21 +0100

    extract enums to variables

--- backend/src/models/Settings.js
+++ backend/src/models/Settings.js
@@ -2,15 +2,18 @@ import mongoose from 'mongoose';
 import Joi from 'joi';
 import { LIGHT_THEME, DARK_THEME } from '../constants/themes';
 
+const themes = [LIGHT_THEME, DARK_THEME];
+const possibleLengths = [3, 5, 7];
+
 const settingsSchema = new mongoose.Schema({
-  appTheme: { type: String, enum: [LIGHT_THEME, DARK_THEME], default: LIGHT_THEME },
-  soonExpiringFilterLength: { type: Number, enum: [3, 5, 7], default: 3 },
+  appTheme: { type: String, enum: themes, default: LIGHT_THEME },
+  soonExpiringFilterLength: { type: Number, enum: possibleLengths, default: 3 },
 });
 
 export const settingsValidator = (req, res, next) => {
   const schema = Joi.object({
-    appTheme: Joi.string().valid(LIGHT_THEME, DARK_THEME),
-    soonExpiringFilterLength: Joi.number().valid(3, 5, 7),
+    appTheme: Joi.string().valid(...themes),
+    soonExpiringFilterLength: Joi.number().valid(...possibleLengths),
   });
   const { error } = schema.validate(req.body);
   if (error) {
