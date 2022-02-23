d4c0efb992ce5ad3b47e9bad49fe143717e53a87
Urszuja <urszula.jarych@gmail.com>
2022-02-23 23:08:25 +0100

    add error handler

--- backend/src/routes/api/test.routes.js
+++ backend/src/routes/api/test.routes.js
@@ -8,6 +8,9 @@ const testEndpoint = (router) => {
     res.status(StatusCodes.OK).send();
     console.log('delete request!');
   });
+  router.get('/error', (_req, res) => {
+    res.statusCode(404);
+  });
 };
 
 export default testEndpoint;
