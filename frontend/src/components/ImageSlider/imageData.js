d79b5e880183287527c9eaa2d9352dbe3818e67c
Sylwia <mangoweb@wp.pl>
2022-03-02 21:16:12 +0100

    add screenshots from the application and reduce the number of slides

--- frontend/src/components/ImageSlider/imageData.js
+++ frontend/src/components/ImageSlider/imageData.js
@@ -3,7 +3,7 @@ import { APP_NAME } from '../../constants/labels';
 const SLIDE_1_PATH = './slide-1.jpg';
 // https://www.pexels.com/photo/addiction-candy-face-portrait-5723610/
 // Photo by cottonbro from Pexels
-const SLIDE_1_TEXT = 'Are you sometimes overwhelmed by the amount of information about your medications?';
+const SLIDE_1_TEXT = 'Are you sometimes overwhelmed by \nthe amount of information about your medications?';
 
 const SLIDE_2_PATH = './slide-2.jpg';
 // https://unsplash.com/photos/Y14ONzYtxb4
@@ -13,70 +13,65 @@ const SLIDE_2_TEXT = 'Do you forget to take your medication on time?';
 const SLIDE_3_PATH = './slide-3.jpg';
 // https://www.pexels.com/photo/20-mg-label-blister-pack-208512/
 // Photo by Pixabay. Free to use. No attribution required.
-const SLIDE_3_TEXT = "You don't know which medications have expired?";
-
-const SLIDE_4_PATH = './slide-4.jpg';
 // https://unsplash.com/photos/byGTytEGjBo
 // Photo by <a href="https://unsplash.com/@nci?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">National Cancer Institute</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-const SLIDE_4_TEXT = 'And when should you visit the pharmacy to buy new ones?';
+const SLIDE_3_TEXT =
+  "You don't know which medications have expired?\nAnd when should you visit the pharmacy to buy new ones?";
 
-const SLIDE_5_PATH = './slide-5.jpg';
+const SLIDE_4_PATH = './slide-4.jpg';
 // https://freerangestock.com/photos/138316/cure-pill-help-pill.html
 // Photo by <a href="https://freerangestock.com/photographer/sapiduduk/4815">sapiduduk</a> on <a href="https://freerangestock.com/photographer/sapiduduk/4815">freerangestock.com</a>
-const SLIDE_5_TEXT = 'You need a help?';
+const SLIDE_4_TEXT = 'You need a help?';
 
-const SLIDE_6_PATH = './slide-6.jpg';
+const SLIDE_5_PATH = './slide-5.jpg';
 // https://unsplash.com/photos/ZsNO9MhKXPU
 // Photo by <a href="https://unsplash.com/@lilartsy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">lilartsy</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-const SLIDE_6_TEXT = 'Now smile :)';
+const SLIDE_5_TEXT = `Now smile :)\n${APP_NAME} application will help you!`;
 
-const SLIDE_7_PATH = './slide-7.jpg';
-const SLIDE_7_TEXT = `${APP_NAME} application will help you :)`;
-
-const SLIDE_8_PATH = './slide-8.jpg';
+const SLIDE_6_PATH = './slide-6.jpg';
 // https://unsplash.com/photos/1TL8AoEDj_c
 // Photo by <a href="https://unsplash.com/@laurynasm?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Laurynas Mereckas</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-const SLIDE_8_TEXT = 'Easily organize your medications';
+const SLIDE_6_TEXT = 'Easily organize your medications';
 
-const SLIDE_9_PATH = './slide-9.jpg';
+const SLIDE_7_PATH = './slide-7.jpg';
 // https://unsplash.com/photos/cfIkM5JgGgE
 // Photo by <a href="https://unsplash.com/@ksyfffka07?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ksenia Yakovleva</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-const SLIDE_9_TEXT = 'Check when you should take your medication';
+const SLIDE_7_TEXT = 'Check when you should take your medication';
 
-const SLIDE_10_PATH = './slide-10.jpg';
+const SLIDE_8_PATH = './slide-8.jpg';
 // https://www.pexels.com/photo/anonymous-female-with-pills-in-hand-and-bottle-6798730/
 // Photo by Michelle Leman from Pexels
-const SLIDE_10_TEXT = 'Check the dosage of the drug';
+const SLIDE_8_TEXT = 'Check the dosage of the drug';
 
-const SLIDE_11_PATH = './slide-11.jpg';
+const SLIDE_9_PATH = './slide-9.jpg';
 // https://unsplash.com/photos/5vl1eKNp98s
 // Photo by <a href="https://unsplash.com/@charissek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Charisse Kenion</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-const SLIDE_11_TEXT = 'Check the expiry date of your medications';
+const SLIDE_9_TEXT = 'Check the expiry date of your medications';
 
-const SLIDE_12_PATH = './slide-12.jpg';
+const SLIDE_10_PATH = './slide-10.jpg';
 // https://www.pexels.com/photo/magnifier-placed-near-green-pills-7788340/
 // Photo by Ivan Babydov from Pexels
-const SLIDE_12_TEXT = 'Find knowledge about drugs in the lexicon';
+const SLIDE_10_TEXT = 'Find knowledge about drugs in the lexicon';
 
-const SLIDE_13_PATH = './slide-13.jpg';
+const SLIDE_11_PATH = './slide-11.jpg';
 // https://www.pexels.com/pl-pl/zdjecie/marketing-mezczyzna-kreatywny-biuro-5310566/
 // Zdjęcie autorstwa Malte Luk z Pexels
-const SLIDE_13_TEXT = 'And even more stuff you will find in this app';
+const SLIDE_11_TEXT = 'And even more stuff';
 
-const SLIDE_14_PATH = './slide-14.jpg';
+const SLIDE_12_PATH = './slide-12.jpg';
 // https://unsplash.com/photos/aXbfOFQ_juU
 // Photo by <a href="https://unsplash.com/@lunarts?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Volodymyr Hryshchenko</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-const SLIDE_14_TEXT = 'Information about all your medications in one place';
+const SLIDE_12_TEXT = 'Information about all your medications in one place';
 
-const SLIDE_15_PATH = './slide-15.jpg';
+const SLIDE_13_PATH = './slide-13.jpg';
 // https://unsplash.com/photos/mN5-yjVGBAI
 // Photo by <a href="https://unsplash.com/@martafilipczyk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Marta Filipczyk</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-const SLIDE_15_TEXT = `${APP_NAME} always with you supporting your health!`;
+const SLIDE_13_TEXT = `${APP_NAME} always with you supporting your health!`;
 
-const SLIDE_16_PATH = './slide-16.jpg';
+const SLIDE_14_PATH = './slide-14.jpg';
 // https://www.kindpng.com/imgv/ihhiiTi_register-now-clipart-png-transparent-png/
 // Photo by <a href="https://www.kindpng.com/userpngs/18057/">Netherlands</a> on <a href="https://www.kindpng.com/imgv/ihhiiTi_register-now-clipart-png-transparent-png/">KindPNG</a>
-const SLIDE_16_TEXT = '';
+const SLIDE_14_TEXT = '';
 
 const imageData = [
   {
@@ -173,20 +168,6 @@ const imageData = [
   {
     imagePathOrUrl: SLIDE_14_PATH,
     text: SLIDE_14_TEXT,
-    get alt() {
-      return this.text;
-    },
-  },
-  {
-    imagePathOrUrl: SLIDE_15_PATH,
-    text: SLIDE_15_TEXT,
-    get alt() {
-      return this.text;
-    },
-  },
-  {
-    imagePathOrUrl: SLIDE_16_PATH,
-    text: SLIDE_16_TEXT,
     get alt() {
       return this.text;
     },
