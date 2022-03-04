4115014e5fdb720e2e71b3687bdc2fe1a1544843
MariaBanaszkiewicz <xmox@interia.pl>
2022-03-04 13:31:24 +0100

    add getting settings from profile

--- backend/src/routes/api/settings.routes.js
+++ backend/src/routes/api/settings.routes.js
@@ -1,9 +1,19 @@
 import { StatusCodes } from 'http-status-codes';
+import Profile from '../../models/Profile';
 
 const settingsEndpoint = (router) => {
-  router.get('/settings', (_req, res) => {
-    res.status(StatusCodes.OK).send({ message: 'Hello from the server' });
+  router.get('/settings/:id', getProfile, (_req, res) => {
+    res.status(StatusCodes.OK).send(res.settings);
   });
 };
+
+async function getProfile(req, res, next) {
+  const profile = await Profile.findById(req.params.id);
+  if (!profile) {
+    return res.status(StatusCodes.NOT_FOUND).send();
+  }
+  res.settings = profile.settings;
+  next();
+}
 
 export default settingsEndpoint;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import testEndpoint from './api/test.routes.js';
+import settingsEndpoint from './api/settings.routes.js';
 
 const router = Router();
 
 testEndpoint(router);
+settingsEndpoint(router);
 
 export default router;
