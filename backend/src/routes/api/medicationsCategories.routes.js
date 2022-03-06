9ec340ec58d2c1850dca07d964e07612fa94d07e
Urszuja <urszula.jarych@gmail.com>
2022-03-06 20:35:09 +0100

    add medication categories route - get all

---
+++ backend/src/routes/api/medicationsCategories.routes.js
@@ -0,0 +1,22 @@
+import MedicationCategory from '../../../models/MedicationCategory';
+import { StatusCodes } from 'http-status-codes';
+import ExpressError from '../../../middlewares/ExpressError';
+
+const MedicationCategoryRoutes = (router) => {
+  router.get('/categories', getAllMedicationCategories);
+};
+
+async function getAllMedicationCategories(req, res, next) {
+  try {
+    const medicationCategories = await MedicationCategory.find({});
+    if (!medicationCategories) {
+      throw new ExpressError('No medication categories found', 404);
+    }
+    res.status(StatusCodes.OK).send({ medicationCategories });
+  } catch (err) {
+    next(err);
+  }
+  next();
+}
+
+export default MedicationCategoryRoutes;
