4119f0a0468e14881bbb87a8d1f36694020fe538
Sylwia <mangoweb@wp.pl>
2022-03-09 22:36:23 +0100

    create registerNewUser function

--- backend/src/middlewares/userMiddlewares.js
+++ backend/src/middlewares/userMiddlewares.js
@@ -13,31 +13,36 @@ export const INITIAL_PROFILE = {
 
 export async function postUser(req, res, next) {
   try {
-    const existingUser = await User.findOne({ username: req.body.username });
-    if (existingUser) {
-      throw new ExpressError('User already exists', StatusCodes.CONFLICT);
-    }
-
-    const user = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    const savedUser = await user.save();
-
-    const profile = new Profile({
-      ...INITIAL_PROFILE,
-      user: savedUser._id,
-    });
-    const savedProfile = await profile.save();
-
-    const savedUserWithProfileRef = await User.updateOne(
-      { username: req.body.username },
-      { $set: { profileRef: savedProfile._id } },
-    );
-
-    res.status(StatusCodes.CREATED).json(savedUserWithProfileRef);
+    const newUser = await registerNewUser(req.body);
+    res.status(StatusCodes.CREATED).json(newUser);
   } catch (err) {
     next(err);
   }
+}
+
+async function registerNewUser(userData) {
+  const existingUser = await User.findOne({ username: userData.username });
+  if (existingUser) {
+    throw new ExpressError('User already exists', StatusCodes.CONFLICT);
+  }
+
+  const user = new User({
+    username: userData.username,
+    email: userData.email,
+    password: userData.password,
+  });
+  const savedUser = await user.save();
+
+  const profile = new Profile({
+    ...INITIAL_PROFILE,
+    user: savedUser._id,
+  });
+  const savedProfile = await profile.save();
+
+  const savedUserWithProfileRef = await User.updateOne(
+    { username: userData.username },
+    { $set: { profileRef: savedProfile._id } },
+  );
+
+  return savedUserWithProfileRef;
 }
