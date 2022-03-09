b9db8822b5451812943d61a15babba60f50e3ad6
Sylwia <mangoweb@wp.pl>
2022-03-09 15:33:47 +0100

    extract middlewares to a separate file

--- backend/src/middlewares/userMiddlewares.js
+++ backend/src/middlewares/userMiddlewares.js
@@ -1,8 +1,9 @@
 import User from '../models/User.js';
+import Profile from '../models/Profile.js';
 import { StatusCodes } from 'http-status-codes';
 import ExpressError from './ExpressError.js';
 
-export default async function getUserById(req, res, next) {
+export async function getUserById(req, res, next) {
   let user;
   try {
     user = await User.findById(req.params.id);
@@ -15,4 +16,27 @@ export default async function getUserById(req, res, next) {
 
   res.user = user;
   next();
+}
+
+export async function getOneUser(req, res) {
+  res.status(StatusCodes.OK).json(res.user);
+}
+
+export async function getAllUsers(req, res, next) {
+  try {
+    const users = await User.find();
+    res.status(StatusCodes.OK).json(users);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function deleteUser(req, res, next) {
+  try {
+    await Profile.find({ user: res.user._id }).deleteOne();
+    await res.user.deleteOne();
+    res.status(StatusCodes.OK).json({ message: 'User deleted' });
+  } catch (err) {
+    next(err);
+  }
 }
--- backend/src/routes/api/users.routes.js
+++ backend/src/routes/api/users.routes.js
@@ -1,35 +1,11 @@
-import { StatusCodes } from 'http-status-codes';
-import User, { userValidator } from '../../models/User.js';
-import Profile from '../../models/Profile.js';
-import getUserById from '../../middlewares/userMiddlewares.js';
+import { getUserById, getOneUser, getAllUsers, deleteUser } from '../../middlewares/userMiddlewares.js';
 
-const UserRoutes = (router) => {
-  router.get('/users/:id', getUserById, async (req, res) => {
-    res.status(StatusCodes.OK).json(res.user);
-  });
+const userRoutes = (router) => {
+  router.get('/users/:id', getUserById, getOneUser);
 
   router.get('/users', getAllUsers);
 
   router.delete('/users/:id', getUserById, deleteUser);
-
-  async function getAllUsers(req, res, next) {
-    try {
-      const users = await User.find();
-      res.status(StatusCodes.OK).json(users);
-    } catch (err) {
-      next(err);
-    }
-  }
-
-  async function deleteUser(req, res, next) {
-    try {
-      await Profile.find({ user: res.user._id }).deleteOne();
-      await res.user.deleteOne();
-      res.status(StatusCodes.OK).json({ message: 'User deleted' });
-    } catch (err) {
-      next(err);
-    }
-  }
 };
 
-export default UserRoutes;
+export default userRoutes;
