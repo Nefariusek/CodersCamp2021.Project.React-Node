bdf95df1247bcf1308c1b3f72f551620883dbd1d
Sylwia <mangoweb@wp.pl>
2022-03-09 13:55:20 +0100

    create delete, get one and get all requests for user

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import testEndpoint from './api/test.routes.js';
+import UserRoutes from './api/users.routes.js';
 
 const router = Router();
 
 testEndpoint(router);
+UserRoutes(router);
 
 export default router;
