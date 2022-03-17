3cf6b3676c8d6a8c82900071856061a5b9800b34
GRosza <grzegorz.roszak@coderscrew.pl>
2022-03-17 20:29:41 +0100

    delete code from another branch

--- backend/src/routes/api/medication.routes.js
+++ backend/src/routes/api/medication.routes.js
@@ -1,54 +1,9 @@
 import Medication, { medicationValidator } from '../../models/Medication.js';
 import { StatusCodes } from 'http-status-codes';
-import ExpressError from '../../middlewares/ExpressError.js';
 
 const MedicationRoutes = (router) => {
-  router.get('/me/meds', getAllMedications);
-  router.post('/me/meds', medicationValidator, postMedication);
   router.delete('me/meds/:id', deleteMedication);
 };
-
-async function getAllMedications(_req, res, next) {
-  try {
-    const medications = await Medication.find({});
-    if (!medications) {
-      throw new ExpressError('No medication found', 404);
-    }
-    res.status(200).send({ medications });
-  } catch (err) {
-    next(err);
-  }
-  next();
-}
-
-async function postMedication(req, res, next) {
-  try {
-    const newMedication = await addNewMedication(req.body);
-    res.status(StatusCodes.CREATED).json(newMedication);
-  } catch (err) {
-    next(err);
-  }
-}
-
-async function addNewMedication(req, _res, _next) {
-  const { nameOfMedication, quantity, addDate, dosage, category, expirationDate, profile } = req.body;
-  const existingMedication = await Medication.findOne({ nameOfMedication: nameOfMedication });
-
-  if (existingMedication) {
-    throw new ExpressError('Medication already in database', StatusCodes.CONFLICT);
-  }
-
-  const medication = new Medication({
-    nameOfMedication,
-    quantity,
-    addDate,
-    dosage,
-    category,
-    expirationDate,
-    profile,
-  });
-  await medication.save();
-}
 
 export async function deleteMedication(req, res, next) {
   try {
