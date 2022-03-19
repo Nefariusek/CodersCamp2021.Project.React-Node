7d824780d36217619ccd8420579f8037acc90077
GRosza <grzegorz.roszak@coderscrew.pl>
2022-03-19 19:35:41 +0100

    add patch route

--- backend/src/middlewares/medicationMiddlewares.js
+++ backend/src/middlewares/medicationMiddlewares.js
@@ -2,6 +2,21 @@ import { StatusCodes } from 'http-status-codes';
 import Medication from '../models/Medication.js';
 import ExpressError from '../../middlewares/ExpressError';
 
+async function patchMedication(req, res, next) {
+  try {
+    const currentMedication = await Medication.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!currentMedication) {
+      throw new Error('Medication not found');
+    } else {
+      res.status(StatusCodes.OK).send(currentMedication);
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { patchMedication };
+
 async function deleteMedication(req, res, next) {
   try {
     const deletedMed = await Medication.findByIdAndDelete(req.params.id);
--- backend/src/routes/api/medicationRoutes.js
+++ backend/src/routes/api/medicationRoutes.js
@@ -1,4 +1,4 @@
-import { deleteMedication } from '../../middlewares/medicationMiddlewares.js';
+import { deleteMedication, patchMedication } from '../../middlewares/medicationMiddlewares.js';
 import { medicationValidator } from '../../models/Medication.js';
 
 const medicationRoutes = async (router) => {
