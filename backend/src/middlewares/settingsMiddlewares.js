f171b8eedff7da760a9ee5b3768f29408106c7cc
MariaBanaszkiewicz <xmox@interia.pl>
2022-03-21 17:19:25 +0100

    add errors in settings middlewares

--- backend/src/middlewares/settingsMiddlewares.js
+++ backend/src/middlewares/settingsMiddlewares.js
@@ -1,35 +1,41 @@
 import { StatusCodes } from 'http-status-codes';
 import Profile from '../models/Profile.js';
 import Settings from '../models/Settings.js';
+import ExpressError from './ExpressError.js';
 
-export async function getSettings(req, res) {
-  let profile;
-  let settings;
+export async function getSettings(req, res, next) {
+  const profile = getProfileById(req.params.id, next);
   try {
-    profile = await Profile.findById(req.params.id);
+    const settings = await Settings.findById(profile.settings);
+    if (!profile) {
+      throw new ExpressError("Can't find profile", StatusCodes.NOT_FOUND);
+    }
+    res.status(StatusCodes.OK).send(settings);
   } catch (err) {
     return next(err);
   }
-  try {
-    settings = await Settings.findById(profile.settings);
-  } catch (err) {
-    return next(err);
-  }
-  res.status(StatusCodes.OK).send(settings);
 }
 
-export async function patchSettings(req, res) {
-  let profile;
-  let settings;
+export async function patchSettings(req, res, next) {
+  const profile = getProfileById(req.params.id, next);
   try {
-    profile = await Profile.findById(req.params.id);
+    const settings = await Settings.findByIdAndUpdate(profile.settings, req.body, { new: true });
+    if (!settings) {
+      throw new ExpressError("Can't update settings", StatusCodes.NOT_MODIFIED);
+    }
+    res.status(StatusCodes.OK).send(settings);
   } catch (err) {
     return next(err);
   }
-  try {
-    settings = await Settings.findByIdAndUpdate(profile.settings, req.body, { new: true });
-  } catch (err) {
-    return next(err);
-  }
-  res.status(StatusCodes.OK).send(settings);
 }
+
+const getProfileById = async (id, next) => {
+  try {
+    const profile = await Profile.findById(id);
+    if (!profile) {
+      throw new ExpressError("Can't find profile", StatusCodes.NOT_FOUND);
+    }
+  } catch (err) {
+    return next(err);
+  }
+};
